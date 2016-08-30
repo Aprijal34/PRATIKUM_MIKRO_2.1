61a87bf3767c079cda53ad030a22cf9be2efa4a3
Aprijal <iskandaraprijal@gmail.com>
2016-08-30 11:08:24 +0700

    Update PRAKTIKUM_MIKRO 2.1.ino

--- PRAKTIKUM_MIKRO 2.1.ino
+++ PRAKTIKUM_MIKRO 2.1.ino
@@ -1,5 +1,5 @@
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
   pinMode(8,OUTPUT );
   pinMode(9,OUTPUT );
   pinMode(10,OUTPUT );
@@ -8,13 +8,12 @@ void setup() {
   pinMode(13,1 );
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
   digitalWrite(8,HIGH);
   digitalWrite(9,HIGH);
   digitalWrite(10,1);
   digitalWrite(11,LOW);
   digitalWrite(12,LOW);
   digitalWrite(13,0);
-
 }
